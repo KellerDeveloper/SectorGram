e20c2c7bc580a24b583d376f1633cbd8c733bd07
Keller Dmitry <kellerdimka@gmail.com>
2026-03-01 14:57:35 +0300

    Add TypeScript definitions for Yandex Maps API

---
+++ sector-web/src/types/yandex-maps.d.ts
@@ -0,0 +1,35 @@
+/** Типы для глобального API Яндекс.Карт (script api-maps.yandex.ru/2.1) */
+type YandexMapInstance = {
+  geoObjects: { add: (obj: unknown) => void; remove: (obj: unknown) => void };
+  events: { add: (type: string, handler: (e: { get: (key: string) => number[] }) => void) => void };
+  setCenter: (center: number[]) => void;
+  destroy: () => void;
+};
+
+declare global {
+  interface Window {
+    ymaps?: {
+      ready: (cb: () => void) => void;
+      Map: new (
+        element: string | HTMLElement,
+        state: { center: number[]; zoom: number },
+        options?: object
+      ) => YandexMapInstance;
+      Placemark: new (coords: number[], properties?: object, options?: object) => unknown;
+      geocode: (
+        query: string | number[],
+        options?: object
+      ) => Promise<{
+        geoObjects: {
+          get: (index: number) => {
+            geometry: { getCoordinates: () => number[] };
+            properties: { get: (key: string) => string };
+          };
+          getLength: () => number;
+        };
+      }>;
+    };
+  }
+}
+
+export {};
